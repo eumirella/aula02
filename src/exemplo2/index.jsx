000bc0e4224d05ec636cc592c29ba1459edcd742
eumirella <mirellaaa.silva2006@gmail.com>
2024-09-30 14:30:12 -0400

    30.09
    30.09

--- src/exemplo2/index.jsx
+++ src/exemplo2/index.jsx
@@ -2,23 +2,28 @@ import { useEffect, useState } from "react";
 
 export default function App() {
   
-  const [??,??] = useState([]);
+  const [usuario, setUsuario] = useState([]);
 
   useEffect(() => { 
-
     const buscarUsuario  = async () => {
-                    = await fetch('');
-        const dados =                ;
+        const resposta = await fetch('https://jsonplaceholder.typicode.com/todos');
+        const dados = await resposta.json();
         setUsuario(dados);
     }
     buscarUsuario();
-  }, //complete o código);
+  }, []);
 
   return (
     <>
-      <h1>Usuário</h1>
+      <h1>Usuario</h1>
       <ul>
-        {}
+        {usuario.map(produto => (
+          <li key={produto.id}>
+            <h2>{produto.title}</h2>
+            <p>{produto.userId}</p>
+            <p>{produto.completed}</p>
+          </li>
+        ))}
       </ul>
     </>
   );
